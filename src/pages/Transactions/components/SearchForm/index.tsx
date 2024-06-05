f13ccef4500a4af274418deb01865fa3a8104318
ruivergani <ruiverganineto@gmail.com>
2024-06-05 20:55:19 +0100

    Visual Structure SearchForm

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,15 @@
+import { MagnifyingGlass } from "phosphor-react";
+import { SearchFormContainer } from "./styles";
+
+
+export function SearchForm() {
+  return (
+    <SearchFormContainer>
+      <input type="text" placeholder="Search for a transaction" />
+      <button>
+        <MagnifyingGlass size={20} />
+        Search
+      </button>
+    </SearchFormContainer>
+  )
+}
---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,41 @@
+import { styled } from 'styled-components';
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input{
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background-color: ${props => props.theme['gray-900']};
+    color: ${props => props.theme['gray-300']};
+    padding: 13px 20px;
+
+    &::placeholder{
+      color: ${props => props.theme['gray-500']};
+    }
+  }
+
+  button{
+    display: flex;
+    align-items: center;
+    gap: 0.7rem;
+    padding: 13px 32px;
+    border-radius: 6px;
+    border: 1px solid ${props => props.theme['green-300']};
+    background-color: transparent;
+    font-weight: 700;
+    line-height: 160%; /* 25.6px */
+    color: ${props => props.theme['green-300']};
+    cursor: pointer;
+    transition: all .2s;
+
+    &:hover{
+      color: ${props => props.theme.white};
+      border-color: ${props => props.theme['green-500']};
+      background-color: ${props => props.theme['green-500']};
+    }
+  }
+
+`;
