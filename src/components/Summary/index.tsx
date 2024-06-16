599aba23296402cb0ab2ebb6c451bcff2ce335ed
ruivergani <ruiverganineto@gmail.com>
2024-06-16 19:10:54 +0100

    API Connection - Creating hook for summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,12 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SummaryCard, SummaryContainer } from "./style";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
 
-  // {income: 0, outcome: 0, total: 0} => reduzir o array (transactions) a uma nova estrutura de dados
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      // Check if the transaction is of type 'income'
-      if (transaction.type === 'income') {
-        // Add the transaction price to the income and total
-        accumulator.income += transaction.price;
-        accumulator.total += transaction.price;
-      } else {
-        // Otherwise, add the transaction price to the outcome and subtract it from the total
-        accumulator.outcome += transaction.price;
-        accumulator.total -= transaction.price;
-      }
-      // Return the accumulator for the next iteration
-      return accumulator;
-    },
-    {
-      // Initial value of the accumulator
-      income: 0,
-      outcome: 0,
-      total: 0
-    }
-  );
+  const summary = useSummary(); // importing our own hook
+
   return (
     <SummaryContainer>
       <SummaryCard>
---
+++ src/hooks/useSummary.tsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../contexts/TransactionsContext";
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  // {income: 0, outcome: 0, total: 0} => reduzir o array (transactions) a uma nova estrutura de dados
+  const summary = transactions.reduce(
+    (accumulator, transaction) => {
+      // Check if the transaction is of type 'income'
+      if (transaction.type === 'income') {
+        // Add the transaction price to the income and total
+        accumulator.income += transaction.price;
+        accumulator.total += transaction.price;
+      } else {
+        // Otherwise, add the transaction price to the outcome and subtract it from the total
+        accumulator.outcome += transaction.price;
+        accumulator.total -= transaction.price;
+      }
+      // Return the accumulator for the next iteration
+      return accumulator;
+    },
+    {
+      // Initial value of the accumulator
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  );
+
+  return summary;
+}
