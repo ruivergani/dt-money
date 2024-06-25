f4d6a534bf240d3daefb06a32fcc30ac9efa1d2d
ruivergani <ruiverganineto@gmail.com>
2024-06-25 09:27:15 +0100

    changes using useMemo

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -8,31 +8,28 @@ export function useSummary() {
   });
 
   // {income: 0, outcome: 0, total: 0} => reduzir o array (transactions) a uma nova estrutura de dados
-  const summary = useMemo(() => {
-    transactions.reduce(
-      (accumulator, transaction) => {
-        // Check if the transaction is of type 'income'
-        if (transaction.type === 'income') {
-          // Add the transaction price to the income and total
-          accumulator.income += transaction.price;
-          accumulator.total += transaction.price;
-        } else {
-          // Otherwise, add the transaction price to the outcome and subtract it from the total
-          accumulator.outcome += transaction.price;
-          accumulator.total -= transaction.price;
-        }
-        // Return the accumulator for the next iteration
-        return accumulator;
-      },
-      {
-        // Initial value of the accumulator
-        income: 0,
-        outcome: 0,
-        total: 0
+  const summary = transactions.reduce(
+    (accumulator, transaction) => {
+      // Check if the transaction is of type 'income'
+      if (transaction.type === 'income') {
+        // Add the transaction price to the income and total
+        accumulator.income += transaction.price;
+        accumulator.total += transaction.price;
+      } else {
+        // Otherwise, add the transaction price to the outcome and subtract it from the total
+        accumulator.outcome += transaction.price;
+        accumulator.total -= transaction.price;
       }
-    );
-  },[transactions]
-  )
+      // Return the accumulator for the next iteration
+      return accumulator;
+    },
+    {
+      // Initial value of the accumulator
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  );
 
   return summary;
 }
